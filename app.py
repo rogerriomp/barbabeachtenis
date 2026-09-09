0fd81e41a8e9103b840a64587a4bdfd2ba251ab8
Rogério de Aquino Silva <aquino@oais.com>
2026-09-09 17:24:08 -0300

    Configurações atualizadas do socketio

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from datetime import datetime
 from dotenv import load_dotenv
 from contextlib import contextmanager
 
-from gevent import monkey
-monkey.patch_all()
+#from gevent import monkey
+#monkey.patch_all()
 
 from flask import Flask, render_template, request, jsonify, session
 from flask_socketio import SocketIO, emit
@@ -20,7 +20,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'chave_super_secreta_barba_beach_2026')
 
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', '')
 DATABASE_URL = os.environ.get('DATABASE_URL', '')
@@ -412,4 +412,4 @@ def handle_connect():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
